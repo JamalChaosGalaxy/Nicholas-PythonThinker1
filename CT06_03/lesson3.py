bbb72fcf7d5c619d9d0adc8de71c7ce01b44c1af
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-01-26 04:08:55 +0000

    Jan 26, 2025, 4:08 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,47 +1,48 @@
-# # # print("Hello from lesson 3")
+# # # # print("Hello from lesson 3")
 
-# i = 1
+# # i = 1
 
-# while i < 3:
-#     1 = input ("How many colors are in the rainbow")
-#     if 1 == 7:
-#         print("Correct")
-#         2 = input("How much is 4 + 4")
-#     else:
-#         print("Incorrect")
-#     if 2 == 8:
-#         print("Correct")
-#         3 = input ("How much is 2 + 2")
-#     else:
-#         print("Incorrect")
-#     if 3 == 4:
-#         print("Correct you finished the test")
-#         break
-#     else:
-#         print("Incorrect")
+# # while i < 3:
+# #     1 = input ("How many colors are in the rainbow")
+# #     if 1 == 7:
+# #         print("Correct")
+# #         2 = input("How much is 4 + 4")
+# #     else:
+# #         print("Incorrect")
+# #     if 2 == 8:
+# #         print("Correct")
+# #         3 = input ("How much is 2 + 2")
+# #     else:
+# #         print("Incorrect")
+# #     if 3 == 4:
+# #         print("Correct you finished the test")
+# #         break
+# #     else:
+# #         print("Incorrect")
 
-score = 0
-answer = input("What is the color of the orange")
-while(answer != "orange"):
-    print("Wrong answer try again noob")
-    answer = input("What is the color of the orange")
+# score = 0
+# answer = input("What is the color of the orange")
+# while(answer != "orange"):
+#     print("Wrong answer try again noob")
+#     answer = input("What is the color of the orange")
 
-print("Correct")
-score = score + 1
+# print("Correct")
+# score = score + 1
 
-answer = input("What is the color of the sun")
-while(answer != "white"):
-    print("Wrong answer try again noob")
-    answer = input("What is the color of the sun")
+# answer = input("What is the color of the sun")
+# while(answer != "white"):
+#     print("Wrong answer try again noob")
+#     answer = input("What is the color of the sun")
 
-print("Correct")
-score = score + 1
+# print("Correct")
+# score = score + 1
 
-answer = input("What is the color of an apple")
-while(answer != "red"):
-    print("Wrong answer try again noob")
-    answer = input("What is the color of an apple")
+# answer = input("What is the color of an apple")
+# while(answer != "red"):
+#     print("Wrong answer try again noob")
+#     answer = input("What is the color of an apple")
 
-print("Correct")
-score = score + 1
+# print("Correct")
+# score = score + 1
+
 
