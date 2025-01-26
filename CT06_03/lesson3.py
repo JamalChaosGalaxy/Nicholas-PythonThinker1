50b73b1f7d3038d51b50902ae967d39e6265ca9c
JamalChaosGalaxy <nicholaswg1@gmail.com>
2025-01-26 03:53:14 +0000

    Jan 26, 2025, 3:53 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,45 @@
-print("Hello from lesson 3")
+# # # print("Hello from lesson 3")
+
+# i = 1
+
+# while i < 3:
+#     1 = input ("How many colors are in the rainbow")
+#     if 1 == 7:
+#         print("Correct")
+#         2 = input("How much is 4 + 4")
+#     else:
+#         print("Incorrect")
+#     if 2 == 8:
+#         print("Correct")
+#         3 = input ("How much is 2 + 2")
+#     else:
+#         print("Incorrect")
+#     if 3 == 4:
+#         print("Correct you finished the test")
+#         break
+#     else:
+#         print("Incorrect")
+
+score = 0
+answer = input("What is the color of the orange")
+while(answer != "orange"):
+    print("Wrong answer try again noob")
+    answer = input("What is the color of the orange")
+
+print("Correct")
+score = 
+
+answer = input("What is the color of the sun")
+while(answer != "white"):
+    print("Wrong answer try again noob")
+    answer = input("What is the color of the sun")
+
+print("Correct")
+
+answer = input("What is the color of an apple")
+while(answer != "red"):
+    print("Wrong answer try again noob")
+    answer = input("What is the color of an apple")
+
+print("Correct")
+
